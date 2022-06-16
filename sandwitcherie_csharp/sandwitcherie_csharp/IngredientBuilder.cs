f559ac7821fa9cb4c3446b0fd68f328dcb46a7b8
xof <ljehanno@myges.fr>
2022-06-16 19:23:30 +0200

    :rocket: builder

---
+++ sandwitcherie_csharp/sandwitcherie_csharp/IngredientBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace sandwitcherie_csharp;
+
+public class IngredientBuilder
+{
+    private double Quantity{ get; set; }
+    private string Name { get; set; }
+
+    public IngredientBuilder(double quantity, string name)
+    {
+        Quantity = quantity;
+        Name = name;
+    }
+
+    public IngredientBuilder WithQuantity(double quantity)
+    {
+        Quantity = quantity;
+        return this;
+    }
+    
+    public IngredientBuilder WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+
+    public Ingredient Build()
+    {
+        return new Ingredient(Quantity, Name);
+    }
+}
