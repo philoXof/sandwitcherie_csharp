653c056e1b466638f16d1dd9ac6c46dc28e5eb84
kelyan-9560 <kelyan.bervin@gmail.com>
2022-06-16 19:22:56 +0200

    start builder

---
+++ sandwitcherie_csharp/sandwitcherie_csharp/SandwichBuilder.cs
@@ -0,0 +1,42 @@
+﻿namespace sandwitcherie_csharp;
+
+public class SandwichBuilder
+{
+    private string Name { get; set;}
+    private List<Ingredient> Ingredients { get; set; }
+    private double Price { get; set; }
+
+
+    public SandwichBuilder(string name, List<Ingredient> ingredients, double price)
+    {
+        Name = name;
+        Ingredients = ingredients;
+        Price = price;
+    }
+
+
+    public SandwichBuilder WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+        
+    public SandwichBuilder WithIngredients(List<Ingredient> ingredients)
+    {
+        Ingredients = ingredients;
+        return this;
+    }
+        
+    public SandwichBuilder WithPrice(double price)
+    {
+        Price = price;
+        return this;
+    }
+
+
+    public Sandwich Build()
+    {
+        return new Sandwich(Name, Ingredients, Price);
+    }
+    
+}
